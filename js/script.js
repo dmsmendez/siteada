c177a8c4a6558e775688540fc533ce1c336763bc
Diana Mendes <dms.diana.mendez@gmail.com>
2020-08-11 18:53:15 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//se os campos de email estiverem preenchidos, envie os dados.
+// se não, vamos pedir para preencher
+// operador && seria o e, verificando se as condições foram atendidas
+//acessar valor usamos .value
+//string - todo texto está em  "aspas"
+// igual == diferente!=
+//alert("texto") - que se deu certo or not
+
+if (document.getElementByID("name").value != "" && document.getElementByID("email").value != ""){
+  alert("Prontinho! Você receberá as novidades por e-mail.")
+}else{
+
+}
+
